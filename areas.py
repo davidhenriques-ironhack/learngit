3192ba3b651f738c669eca4aedac71457a057841
davidhenriques-ironhack <david.henriques@ironhack.com>
2020-10-03 17:15:25 +0100

    back to square one

--- areas.py
+++ areas.py
@@ -3,9 +3,7 @@ def square_area(s):
     return s*s
 
 #TODO: Implement function that returns the area of a rectangle of length l and width w
-def rect_area(l,w):
-    return l*w
+#def rect_area(l,w):
 
-#ODO: Implement function that returns the area of a circle with radius r
-def circle_area(r):
-    return 3.14*r*r
+#TODO: Implement function that returns the area of a circle with radius r
+#def circle_area(r):
--- volumes.py
+++
@@ -1,3 +0,0 @@
-#computes the volume of a cube with side s
-def cube_volume(s):
-    s*s*s
