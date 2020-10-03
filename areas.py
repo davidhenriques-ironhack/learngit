9cc56358c9b890653e1f9e4b0e165b82fa6472b6
davidhenriques-ironhack <david.henriques@ironhack.com>
2020-10-03 16:21:27 +0100

    added the first file

---
+++ areas.py
@@ -0,0 +1,9 @@
+#Computes the area of a square of side s
+def square_area(s):
+    return s*s
+
+#TODO: Implement function that returns the area of a rectangle of length l and width width
+#def rect_area(l,w):
+
+#TODO: Implement function that returns the area of a circle with radius r
+#def circle_area(r):
