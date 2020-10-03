00afa4ff58f6def1cef561361be3c626c1027ce7
davidhenriques-ironhack <david.henriques@ironhack.com>
2020-10-03 17:26:42 +0100

    areas.py now has area of circle

--- areas.py
+++ areas.py
@@ -2,8 +2,6 @@
 def square_area(s):
     return s*s
 
-#TODO: Implement function that returns the area of a rectangle of length l and width w
-#def rect_area(l,w):
 
 #TODO: Implement function that returns the area of a circle with radius r
 def circle_area(r):
