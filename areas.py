22bb0d6c7924ef4366ba6c2447785c58bbd3fc82
davidhenriques-ironhack <david.henriques@ironhack.com>
2021-01-11 16:44:39 +0000

    implemented area of square and cube

--- areas.py
+++ areas.py
@@ -1,8 +1,10 @@
 #TODO: Implement function that computes the area of a square of side s
 def square_area(s):
+    return s*s
 
 #TODO: Implement function that returns the area of a cube of side s
 def cube_area(s):
+    return square_area(s)*6 
 
 
 #TODO: Implement function that returns the area of a circle with radius r
