7024729c652ef8c4bf06aebaf686d9e07f8030b6
davidhenriques-ironhack <72150978+davidhenriques-ironhack@users.noreply.github.com>
2021-03-21 16:57:56 +0000

    Update areas.py

--- areas.py
+++ areas.py
@@ -1,12 +1,9 @@
 #TODO: Implement function that computes the area of a square of side s
 def square_area(s):
-    return s*s
 
 #TODO: Implement function that returns the area of a cube of side s
 def cube_area(s):
-    return square_area(s)*6 
 
 
 #TODO: Implement function that returns the area of a circle with radius r
 def circle_area(r):
-    return 3.14*r*r
