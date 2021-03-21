6f9b6f428e1f448457b775bf6ca3be6a633e06f4
davidhenriques-ironhack <72150978+davidhenriques-ironhack@users.noreply.github.com>
2021-03-21 16:59:52 +0000

    Update areas.py

--- areas.py
+++ areas.py
@@ -1,9 +1,11 @@
 #TODO: Implement function that computes the area of a square of side s
 def square_area(s):
 
+#TODO: Implement function that computes the area of a rectangle of sides w and l
+def rectangle_area(w,l):
+
 #TODO: Implement function that returns the area of a cube of side s
 def cube_area(s):
-
 
 #TODO: Implement function that returns the area of a circle with radius r
 def circle_area(r):
