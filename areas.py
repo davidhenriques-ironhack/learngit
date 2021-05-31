c66066fe787d7d6bc73744bd7cb3f16916495074
davidhenriques-ironhack <david.henriques@ironhack.com>
2021-05-31 17:34:42 +0100

    implemented all functions

--- areas.py
+++ areas.py
@@ -4,9 +4,12 @@ def square_area(s):
     
 #TODO: Implement function that computes the area of a rectangle of sides w and l
 def rectangle_area(w,l):
+    return w*l
 
 #TODO: Implement function that returns the area of a cube of side s
 def cube_area(s):
+    6*square_area(s)
 
 #TODO: Implement function that returns the area of a circle with radius r
 def circle_area(r):
+    return 3.14*r*r
